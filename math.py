02b4d5e1c8d8a050aa51868197f9ccfa1af85ac6
yashika5100 <64487313+yashika5100@users.noreply.github.com>
2021-01-29 15:42:43 +0530

    Add files via upload
    Initial commit

---
+++ math.py
@@ -0,0 +1,12 @@
+#Addition
+def add(x,y):
+            return x+y
+#Substraction
+def substract(x,y):
+if(x<y)return ERROR
+                 return x-y 
+def multiply(x,y):
+                 pass
+#change here
+def divide(x,y):
+              pass
